b5ab06f138d31ac444cafc0516d5c5d7ae151745
Mavkun <delacruzmarkjoseph28@gmail.com>
2023-02-28 15:56:34 +0800

    newnews

---
+++ DepEd-LMS-Project/news-right-nav.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		#news-nav-table-container table tr td img{
+			width: 80%;
+		}
+	</style>
+</head>
+<body>
+	<div>
+	<h2>Other News</h2>
+	<div id="news-nav-table-container">
+		<table>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 1 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 2 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 3 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 4 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 5 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 6 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 7 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 8 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 9 Title</h3></a></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><img src="img/news-placeholder.jpg">
+					<a href=""><h3>News 10 Title</h3></a></center>
+				</td>
+			</tr>
+		</table>
+	</div>
+</div>
+</body>
+</html>
--- DepEd-LMS-Project/news.php
+++ DepEd-LMS-Project/news.php
@@ -5,6 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>News</title>
+    <style>
+        #news-div-container{
+            display: flex;
+        }
+        #news-div-container #left-newscontent{
+            flex-basis: 80%;
+        }
+        #news-div-container #right-news-nav{
+            flex-basis: 20%;
+            width: 100%;
+        }
+        #news-div-container #right-news-nav iframe{
+            height: 100%;
+        }
+        .news-rows{
+            padding: 15px;
+        }
+        .news-rows .news-img{
+            width: 80%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .news-rows .news-img img{
+            width: 100%;
+        }
+        .news-rows .news-title{
+            text-align: center;
+            margin: 5px;
+        }
+        .news-rows .news-title a{
+            color: #5f2af0;
+            text-decoration: none;
+        }
+        .news-rows .news-content{
+            width: 70%;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: justify;
+        }
+    </style>
     <?php require 'links.php'; ?>
 
     <?php include 'script.php'; ?>
@@ -17,97 +57,46 @@
             </section>
     <div id="news-container">
         <center>
-            <h1 class="fw-bold">News and Updates<br></h1>
+            <h1 class="fw-bold">News and Updates<br></h1><br>
         </center>
-        <section class="group-card">  
-        <table id="news-table">
-            <tr>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+        <section class="group-card"> 
+
+        <div id="news-div-container">
+            <div id="left-newscontent">
+                <div class="news-rows">
+                    <div class="news-img">
+                        <img src="img/news-sample.jpg ">
                     </div>
-                <td>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+                    <div class="news-title">
+                        <br>
+                            <a href=""><h2>Deped E-Taguyod into E-learning Website Project</h2></a>
                     </div>
-                <td>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div class="news-content">
+                        <p>&emsp;&emsp;&emsp;&emsp;In joint project with JCI Makati, Deped Filipino E-taguyod was into creating E-Learning or Learning Management System (LMS) for guiding teachers into teaching Filipino subject. Lorem ipsum dolor sit amet. Nam magnam quod qui excepturi molestiae ex quidem sint. Non Quis mollitia id iste exercitationem a laboriosam voluptatum in aliquid commodi! <a href="">Read More.</a></p>
                     </div>
-                <td>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+                <div class="news-rows">
+                    <div class="news-img">
+                        <img src="img/handshake.jpg ">
                     </div>
-                <td>    
-            <tr>
-            <tr>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+                    <div class="news-title">
+                        <br>
+                            <a href=""><h2>Deped E-taguyod Filipino Department and JCI Makati</h2></a>
                     </div>
-                <td>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div class="news-content">
+                        <p>&emsp;&emsp;&emsp;&emsp;JCI Makati and Deped E-taguyod join forces in building Learning Management System (LMS). Lorem ipsum dolor sit amet. Nam magnam quod qui excepturi molestiae ex quidem sint. Non Quis mollitia id iste exercitationem a laboriosam voluptatum in aliquid commodi! <a href="">Read More.</a></p>
                     </div>
-                <td>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                <td>
-                <td>
-                    <div class="team-card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="a-bg"><center><a href="">Read More...</a></center></div>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                <td>    
-            <tr>
-        </table>
-    </div>
-    </section> 
+                </div>
+            </div>
+
+            <div id="right-news-nav">
+                <iframe src="news-right-nav.php"></iframe>
+            </div>
+        </div>
+        </section> 
       
     <?php include 'footer.php'; ?>
 </body>
